0e76511f2f4f5af247fac0e29b769b8c96043842
arief <arief>
2020-12-28 05:08:24 +0700

    logo header

--- resources/js/src/layouts/components/Logo.vue
+++ resources/js/src/layouts/components/Logo.vue
@@ -1,4 +1,4 @@
-<!--<template>-->
+<template>
 <!--  <svg viewBox="0 0 139 95" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" height="35">-->
 <!--      <defs>-->
 <!--          <linearGradient x1="100%" y1="10.5120544%" x2="50%" y2="89.4879456%" id="linearGradient-1">-->
@@ -22,6 +22,6 @@
 <!--          </g>-->
 <!--      </g>-->
 <!--  </svg>-->
-<!--</template>-->
+    <img src="@assets/images/logo/logo.png" style="height: 30px" />
+</template>
 
-<img src="@assets/images/logo/logo.png" style="height: 30px" />
