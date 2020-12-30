f3ab18dd607ee072550851461c652dbe8fa4787d
Indra Laksmana <laksmana.d.indra@gmail.com>
2020-12-30 09:43:48 +0700

    feat(unused-codes): clean unused codes

--- resources/js/src/router.js
+++ resources/js/src/router.js
@@ -21,9 +21,6 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import auth from '@/auth/authService'
 
-import firebase from 'firebase/app'
-import 'firebase/auth'
-
 Vue.use(Router)
 
 const router = new Router({
@@ -86,37 +83,15 @@ router.afterEach(() => {
 })
 
 router.beforeEach((to, from, next) => {
-  firebase.auth().onAuthStateChanged(() => {
-
-    // get firebase current user
-    const firebaseCurrentUser = firebase.auth().currentUser
-
-    // if (
-    //     to.path === "/pages/login" ||
-    //     to.path === "/pages/forgot-password" ||
-    //     to.path === "/pages/error-404" ||
-    //     to.path === "/pages/error-500" ||
-    //     to.path === "/pages/register" ||
-    //     to.path === "/callback" ||
-    //     to.path === "/pages/comingsoon" ||
-    //     (auth.isAuthenticated() || firebaseCurrentUser)
-    // ) {
-    //     return next();
-    // }
 
     // If auth required, check login. If login fails redirect to login page
     if (to.meta.authRequired) {
-      if (!(auth.isAuthenticated() || firebaseCurrentUser)) {
+      if (!(auth.isAuthenticated())) {
         router.push({ path: '/pages/login', query: { to: to.path } })
       }
     }
 
     return next()
-    // Specify the current path as the customState parameter, meaning it
-    // will be returned to the application after auth
-    // auth.login({ target: to.path });
-
-  })
 
 })
 
